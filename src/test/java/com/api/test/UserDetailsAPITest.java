8d6332d4990d27ef0f419d3b6d8e8ab4b2cf92ea
Umesh D Parab <umesh.parabcr10@gmail.com>
2026-02-22 12:02:04 +0530

    Base Code For My Automation Framework

---
+++ src/test/java/com/api/test/UserDetailsAPITest.java
@@ -0,0 +1,45 @@
+package com.api.test;
+
+import static io.restassured.RestAssured.given;
+
+import java.io.IOException;
+
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import static com.api.utils.ConfigManager.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+public class UserDetailsAPITest {
+	
+	@Test
+	public void userDetailsAPITest() throws IOException {
+		
+		
+		Header authHeader = new Header("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZmlyc3RfbmFtZSI6ImZkIiwibGFzdF9uYW1lIjoiZmQiLCJsb2dpbl9pZCI6ImlhbWZkIiwibW9iaWxlX251bWJlciI6Ijg4OTk3NzY2NTUiLCJlbWFpbF9pZCI6Im1hcmtAZ21haWwuY29tIiwicGFzc3dvcmQiOiI1ZjRkY2MzYjVhYTc2NWQ2MWQ4MzI3ZGViODgyY2Y5OSIsInJlc2V0X3Bhc3N3b3JkX2RhdGUiOm51bGwsImxvY2tfc3RhdHVzIjowLCJpc19hY3RpdmUiOjEsIm1zdF9yb2xlX2lkIjo1LCJtc3Rfc2VydmljZV9sb2NhdGlvbl9pZCI6MSwiY3JlYXRlZF9hdCI6IjIwMjEtMTEtMDNUMDg6MDY6MjMuMDAwWiIsIm1vZGlmaWVkX2F0IjoiMjAyMS0xMS0wM1QwODowNjoyMy4wMDBaIiwicm9sZV9uYW1lIjoiRnJvbnREZXNrIiwic2VydmljZV9sb2NhdGlvbiI6IlNlcnZpY2UgQ2VudGVyIEEiLCJpYXQiOjE3NzE2MTE4Njh9.SsbMpJiNvdUOhqJCNHFvwFOfwIKtTO556GtWZA-twSg");
+		
+		given()
+			.baseUri(getProperty("BASE_URI"))
+			.contentType(ContentType.JSON)
+			.and()
+			.header(authHeader)
+			.log().uri()
+			.log().method()
+		
+		.when()
+			.get("userdetails")
+			
+		.then()
+			.statusCode(200)
+			.log().all()
+			.and()
+			.body("message",Matchers.equalTo("Success"))
+			.and()
+			.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-schema/UserDetailsResponseSchema.json"));
+		
+	}
+
+}
